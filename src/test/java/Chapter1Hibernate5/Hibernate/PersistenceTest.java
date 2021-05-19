39dcc432b4e1744ea1a8f6be28a36f7409a7e793
YoungMetroid <outsetwaker@gmail.com>
2021-05-19 16:47:10 -0500

    Learning Hibernate

---
+++ src/test/java/Chapter1Hibernate5/Hibernate/PersistenceTest.java
@@ -0,0 +1,71 @@
+package Chapter1Hibernate5.Hibernate;
+
+import com.LearningHibernate.Message;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+public class PersistenceTest {
+
+    private SessionFactory factory = null;
+
+    @BeforeClass
+    public  void setUp()
+    {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+        factory = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    @Test
+    public void saveMessage() {
+        Message message = new Message("Hello, world");
+        try (Session session = factory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            session.persist(message);
+            tx.commit();
+        }
+    }
+
+    @Test(dependsOnMethods = "saveMessage")
+    public void readMessage(){
+        try(Session session = factory.openSession()){
+            List<Message> list = session.createQuery("from Message", Message.class).list();
+
+            assertEquals(list.size(),1);
+            for(Message m: list)
+            {
+                System.out.println(m);
+            }
+        }
+    }
+
+
+    public void CreateDataBase()
+    {
+        try(Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","ghost"))
+        {
+            try(PreparedStatement ps = connection.prepareStatement(
+            "SELECT 'CREATE DATABASE TestDB' " +
+                    "WHERE NOT EXISTS (SELECT FROM pg_database = 'TestDB')\\gexec"
+            ))
+        }
+        catch (SQLException ex)
+        {
+
+        }
+    }
+
+}
